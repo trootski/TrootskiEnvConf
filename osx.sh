f0eb3537b32f66369e16b838bae0b321f8003e6e
John Byrne <john@brando.ie>
2014-11-23 15:37:54 +0000

    Reworking dotfiles

---
+++ osx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Add tab completion for `defaults read|write NSGlobalDomain`
+# You could just use `-g` instead, but I like being explicit
+if [[ $OSTYPE == darwin* ]]; then 
+  complete -W "NSGlobalDomain" defaults
+fi
