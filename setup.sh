5b8eb62a779d5be432eee4c54761bd9b38418c16
John Byrne <john@brando.ie>
2014-11-25 01:25:13 -0800

    Added tmuxinator support

--- setup.sh
+++ setup.sh
@@ -2,40 +2,21 @@
 
 set -e
 
-# Setup the script on a new machine
+[[ ! -e ~/TrootskiEnvConf/backups ]] && mkdir ~/TrootskiEnvConf/backups
 
-if [ "$#" -eq 1 ];then
-    user_home_path=$(eval echo ~$user_home_path$1)
-fi
+for file in ~/.{bashrc,bash_profile,vimrc,tmuxinator}; do
+	if [[ -h "$file" ]]
+	then
+		# File is a symbolic link
+		rm "$file"
+	elif [[ -e "$file" ]]
+	then
+		# File exists, move to the backup folder
+		mv "$file" ~/TrootskiEnvConf/backups/
+	fi
+	echo "Creating symbolic link from "
+	echo ~/TrootskiEnvConf/$(echo $(basename "$file") | sed "s/^\.//")
+	echo "$file"
+	ln -s ~/TrootskiEnvConf/$(echo $(basename "$file") | sed "s/^\.//") "$file"
+done
 
-[[ ! -e $user_home_path/backups ]] && mkdir $user_home_path/backups
-
-if [[ -h $user_home_path/.bashrc ]]
-then
-	rm $user_home_path/.bashrc
-elif [[ -e $user_home_path/.bashrc ]]
-then
-	# Move the current bashrc to the backup folder
-	mv $user_home_path/.bashrc $user_home_path/TrootskiEnvConf/backups/
-fi
-ln -s $user_home_path/TrootskiEnvConf/bashrc $user_home_path/.bashrc
-
-if [[ -h $user_home_path/.bash_profile ]]
-then
-	rm $user_home_path/.bash_profile
-elif [[ -e $user_home_path/.bash_profile ]]
-then
-	# Move the current bash_profile to the backup folder
-	mv $user_home_path/.bash_profile $user_home_path/TrootskiEnvConf/backups/
-fi
-ln -s $user_home_path/TrootskiEnvConf/bash_profile $user_home_path/.bash_profile
-
-if [[ -h $user_home_path/.vimrc ]]
-then
-	rm $user_home_path/.vimrc
-elif [[ -e $user_home_path/.vimrc ]]
-then
-	# Move the current bash_profile to the backup folder
-	mv $user_home_path/.vimrc $user_home_path/TrootskiEnvConf/backups/
-fi
-ln -s $user_home_path/TrootskiEnvConf/vimrc $user_home_path/.vimrc
