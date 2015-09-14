f90cab5b197738c749f5278b538d4168a5dfe764
John Byrne <troot@devserver.dev>
2015-09-14 14:56:52 +0100

    Started script to do ffmpeg export of videos

--- linux.sh
+++ linux.sh
@@ -82,4 +82,73 @@ if ! shopt -oq posix; then
   fi
 fi
 
+function t_ffmpeg_bitrate {
+
+	MP4_FNAME_FULL=$(basename "$1")
+	MP4_FNAME="${MP4_FNAME_FULL%.*}"
+
+	BITRATE=550
+	if [ "$#" -gt 1 ]; then
+		BITRATE="$2"
+	fi
+
+	MP4_WIDTH=1280
+	if [ "$#" -gt 2 ]; then
+		MP4_WIDTH="$3"
+	fi
+
+	MP4_HEIGHT=720
+	if [ "$#" -gt 3 ]; then
+		MP4_HEIGHT="$4"
+	fi
+
+	MP4_OUT="$MP4_FNAME""-b-""$BITRATE""k-""$MP4_WIDTH""x""$MP4_HEIGHT"".mp4"
+
+	#echo "Encoding ""$MP4_FNAME"". Dimensions: ""$MP4_WIDTH"":""$MP4_HEIGHT"". Bitrate (""$BITRATE"")"
+	echo "Creating ""$MP4_OUT"
+
+	# MP4 @ provided bit rate (default: 550)
+	ffmpeg -y -i "$MP4_FNAME_FULL" -c:v libx264 -s "$MP4_WIDTH":"$MP4_HEIGHT" -preset slow -b:v "$BITRATE"k -pass 1 -movflags +faststart -profile:v baseline -level 4.0 -c:a libfdk_aac -b:a 128k -f mp4 /dev/null && ffmpeg -y -i "$MP4_FNAME_FULL" -c:v libx264 -s "$MP4_WIDTH":"$MP4_HEIGHT" -preset slow -b:v "$BITRATE"k -pass 2 -movflags +faststart -profile:v baseline -level 4.0 -c:a libfdk_aac -b:a 128k "$MP4_OUT"
+#
+}
+
+function t_html5_video_suite {
+
+	ORIG_MP4_FNAME_FULL=$(basename "$1")
+	ORIG_MP4_FNAME="${ORIG_MP4_FNAME_FULL%.*}"
+
+	# DASH FILES
+	#t_ffmpeg_bitrate "$1" "350" "320" "240"
+	#t_ffmpeg_bitrate "$1" "550" "480" "360"
+	#t_ffmpeg_bitrate "$1" "900" "720" "480"
+	#t_ffmpeg_bitrate "$1" "1600" "1280" "720"
+
+	MP4_OUT_Q1="$ORIG_MP4_FNAME""-b-350k-320x240.mp4"
+	MP4_OUT_Q2="$ORIG_MP4_FNAME""-b-550k-480x360.mp4"
+	MP4_OUT_Q3="$ORIG_MP4_FNAME""-b-900k-720x480.mp4"
+	MP4_OUT_Q4="$ORIG_MP4_FNAME""-b-1600k-1280x720.mp4"
+
+	OUT_FILE="$ORIG_MP4_FNAME"".mpd"
+
+	MP4Box -dash 2000 -rap -frag-rap -profile onDemand -out "$OUT_FILE" "$MP4_OUT_Q1" "$MP4_OUT_Q2" "$MP4_OUT_Q3" "$MP4_OUT_Q4"
+
+	# HLS segmented file
+	ffmpeg -y -i "$MP4_OUT_Q3" -map 0 -codec:v libx264 -codec:a libfdk_aac -f ssegment -segment_list "$ORIG_MP4_FNAME".m3u8 -segment_list_flags +live -segment_time 10 "$ORIG_MP4_FNAME"%03d.ts
+
+}
+
+function t_convert_all {
+	unset a i
+	while IFS= read -r -d $'\0' file; do
+		echo "$file"
+		a[i++]="$file"
+	done < <(find /media/sf_Sites/showcase.brando.ie/htdocs -type f -name '*.mp4' -print0)
+
+	for f in "${a[@]}"; do
+		MP4_FNAME_FULL=$(basename "$f")
+		MP4_FNAME="${MP4_FNAME_FULL%.*}"
+		cd "$(dirname "$f")"
+		t_html5_video_suite "$MP4_FNAME_FULL"
+	done
+}
 
