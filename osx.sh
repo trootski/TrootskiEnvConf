1511d54482a0a525164de47bdf6a5a6500fc549c
John Byrne <john@brando.ie>
2015-09-15 14:42:54 +0100

    Added a helper function for converting a PNG to JPG

--- osx.sh
+++ osx.sh
@@ -6,3 +6,9 @@ if [[ $OSTYPE == darwin* ]]; then
   complete -W "NSGlobalDomain" defaults
 fi
 
+function t_vlc_conv_snapshot {
+	sips -s format jpeg "$1" -s formatOptions 60 -z 720 1280 --out ~/Desktop/thumbnail.jpg
+	rm "$1"
+}
+
+
