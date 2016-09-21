2d003be768f24c9de45906e81d377efe9266d0bf
Byrne, John <John.Byrne@hmhco.com>
2016-09-21 08:55:47 +0100

    Add t_ffmpeg_bitrate command to osx

--- bash/osx.sh
+++ bash/osx.sh
@@ -19,6 +19,36 @@ function t_vlc_conv_snapshot {
 	rm "$1"
 }
 
+function t_ffmpeg_bitrate {
+
+	MP4_FNAME_FULL=$(basename "$1")
+	MP4_FNAME="${MP4_FNAME_FULL%.*}"
+
+	BITRATE=550
+	if [ "$#" -gt 1 ]; then
+		BITRATE="$2"
+	fi
+
+	MP4_WIDTH=1280
+	if [ "$#" -gt 2 ]; then
+		MP4_WIDTH="$3"
+	fi
+
+	MP4_HEIGHT=720
+	if [ "$#" -gt 3 ]; then
+		MP4_HEIGHT="$4"
+	fi
+
+	MP4_OUT="$MP4_FNAME""-b-""$BITRATE""k-""$MP4_WIDTH""x""$MP4_HEIGHT"".mp4"
+
+	#echo "Encoding ""$MP4_FNAME"". Dimensions: ""$MP4_WIDTH"":""$MP4_HEIGHT"". Bitrate (""$BITRATE"")"
+	echo "Creating ""$MP4_OUT"
+
+	# MP4 @ provided bit rate (default: 550)
+	ffmpeg -y -i "$MP4_FNAME_FULL" -c:v libx264 -s "$MP4_WIDTH":"$MP4_HEIGHT" -preset slow -b:v "$BITRATE"k -pass 1 -movflags +faststart -profile:v baseline -level 4.0 -acodec aac -strict experimental -b:a 128k -f mp4 /dev/null && ffmpeg -y -i "$MP4_FNAME_FULL" -c:v libx264 -s "$MP4_WIDTH":"$MP4_HEIGHT" -preset slow -b:v "$BITRATE"k -pass 2 -movflags +faststart -profile:v baseline -level 4.0 -acodec aac -strict experimental -b:a 128k "$MP4_OUT"
+#
+}
+
 # TextEdit
 # Use Plain Text Mode as Default
 #defaults write com.apple.TextEdit RichText -int 0
