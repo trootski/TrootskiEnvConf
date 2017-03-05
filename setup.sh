13d2e517962bbf5d9e78595ee2ab433113691375
John Byrne <troot1@gmail.com>
2017-03-05 22:35:46 +0000

    Make note of further packages to install via brew

--- setup.sh
+++ setup.sh
@@ -33,9 +33,12 @@ if [[ "$OSTYPE" =~ darwin1[0-9] ]]; then
 	which -s brew
 	if [[ $? = 0 ]] ; then
 		# Install the default packages
-		for pkg in ~/.{bash-completion,git,tmux,node,vim,python}; do
+		for pkg in ~/.{bash-completion,git,tmux,node,python,reattach-to-user-namespace,tig}; do
 			brew install 
 		done
+		brew install ffmpeg --with-fdk-aac
+		brew install vim --wth-override-system-vi --with-python3
+		brew install disk-inventory-x
 	fi
 fi
 
