f0e0b3cc2df3948ae8ee7cc9551e9072e0e971e6
John Byrne <troot1@gmail.com>
2018-03-13 00:30:23 +0000

    chore: include highlighting for man pages

--- bash/exports.sh
+++ bash/exports.sh
@@ -9,6 +9,15 @@ export EDITOR="vim"
 export LC_ALL="en_GB.UTF-8"
 export LANG="en_GB"
 
+# highlighting inside manpages and elsewhere
+export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
+export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
+export LESS_TERMCAP_me=$'\E[0m'           # end mode
+export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
+export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
+export LESS_TERMCAP_ue=$'\E[0m'           # end underline
+export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
+
 ############################################
 # History settings
 #
